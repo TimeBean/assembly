7e1baeeed5cd29eeb2c1714405e59cb7e8735a73
Alexander <alexanderklush8@gmail.com>
2026-01-22 19:41:10 +0300

    git commit -m "refactor: improve readability
    - Replace multiple byte comparisons with reusable strcmp function
    - Separate code into functions
    - Remove dead code
    - Fix potential buffer overflow in newline removal

--- ksh/src/io.asm
+++ ksh/src/io.asm
@@ -6,34 +6,41 @@ section .text
 global print
 global read
 
+; Print string to stdout
+; rsi = buffer pointer
+; rdx = length
 print:
-    mov rax, 1
-    mov rdi, 1
+    mov rax, 1          ; sys_write
+    mov rdi, 1          ; stdout
     syscall
     ret
 
+; Read input from stdin
+; Returns: rax = bytes read
 read:
+    ; Clear buffer
     xor rcx, rcx
-.clear_buf:
+.clear_loop:
     cmp rcx, 128
-    je .done_clear
+    je .read_input
     mov byte [buf+rcx], 0
     inc rcx
-    jmp .clear_buf
-.done_clear:
+    jmp .clear_loop
 
-    mov rax, 0
-    mov rdi, 0
+.read_input:
+    mov rax, 0          ; sys_read
+    mov rdi, 0          ; stdin
     mov rsi, buf
     mov rdx, 128
     syscall
-
-    ; remove \n
+    
+    ; Remove trailing newline if present
     test rax, rax
     jz .done
     dec rax
     cmp byte [buf+rax], 10
     jne .done
     mov byte [buf+rax], 0
+
 .done:
     ret
--- ksh/src/main.asm
+++ ksh/src/main.asm
@@ -6,19 +6,22 @@ section .data
 msg:    db "welcome to ksh!", 10
 msl:    equ $ - msg
 
-shs:    db " > "
-shl:    equ $ - shs
+prompt: db " > "
+prl:    equ $ - prompt
 
-kms:    db 27, "[1;38;5;2;49m", "ksh stands for kulich shell", 27, "[0;39;49m", 10
-mkl:    equ $ - kms
+info_msg: db 27, "[1;38;5;2;49m", "ksh stands for kulich shell", 27, "[0;39;49m", 10
+iml:    equ $ - info_msg
 
-clr:    db 27, '[', '2', 'J', 27, '[', 'H'
-cll:    equ $ - clr
+clear_screen: db 27, '[2J', 27, '[H'
+csl:    equ $ - clear_screen
 
-nln:    db 10
+; Command strings for comparison
+cmd_exit:   db "exit", 0
+cmd_clear:  db "clear", 0
+cmd_info:   db "info", 0
 
 section .bss
-argv    resq 8
+argv:   resq 8
 
 section .text
 _start:
@@ -27,121 +30,167 @@ _start:
     call print
 
 main_loop:
-    mov rsi, shs
-    mov rdx, shl
+    ; Show prompt
+    mov rsi, prompt
+    mov rdx, prl
     call print
-
+    
+    ; Read user input
     call read
     cmp rax, 0
     jle main_loop
+    
+    ; Check for built-in commands
+    call check_builtin
+    test rax, rax
+    jnz main_loop
+    
+    ; Parse and execute external command
+    call parse_args
+    call execute_command
+    jmp main_loop
 
-    cmp byte [buf], 'e'
-    jne .check_clear
-    cmp byte [buf+1], 'x'
-    jne .check_clear
-    cmp byte [buf+2], 'i'
-    jne .check_clear
-    cmp byte [buf+3], 't'
-    jne .check_clear
-    cmp byte [buf+4], 0
-    je _exit
+; Check if input is a built-in command
+; Returns: rax = 1 if built-in handled, 0 otherwise
+check_builtin:
+    ; Check "exit"
+    mov rsi, buf
+    mov rdi, cmd_exit
+    call strcmp
+    test rax, rax
+    jz exit_shell
+    
+    ; Check "clear"
+    mov rsi, buf
+    mov rdi, cmd_clear
+    call strcmp
+    test rax, rax
+    jz do_clear
+    
+    ; Check "info"
+    mov rsi, buf
+    mov rdi, cmd_info
+    call strcmp
+    test rax, rax
+    jz do_info
+    
+    ; Not a built-in command
+    xor rax, rax
+    ret
 
-.check_clear:
-    cmp byte [buf], 'c'
-    jne .check_info
-    cmp byte [buf+1], 'l'
-    jne .check_info
-    cmp byte [buf+2], 'e'
-    jne .check_info
-    cmp byte [buf+3], 'a'
-    jne .check_info
-    cmp byte [buf+4], 'r'
-    jne .check_info
-    cmp byte [buf+5], 0
-    je .do_clear
+do_clear:
+    mov rsi, clear_screen
+    mov rdx, csl
+    call print
+    mov rax, 1
+    ret
 
-.check_info:
-    cmp byte [buf], 'i'
-    jne .parse
-    cmp byte [buf+1], 'n'
-    jne .parse
-    cmp byte [buf+2], 'f'
-    jne .parse
-    cmp byte [buf+3], 'o'
-    jne .parse
-    cmp byte [buf+4], 0
-    je .do_info
+do_info:
+    mov rsi, info_msg
+    mov rdx, iml
+    call print
+    mov rax, 1
+    ret
 
-.parse:
+exit_shell:
+    mov rax, 60         ; sys_exit
+    xor rdi, rdi
+    syscall
+
+; Compare two null-terminated strings
+; rsi = string 1, rdi = string 2
+; Returns: rax = 0 if equal, non-zero otherwise
+strcmp:
+    push rbx
+    xor rbx, rbx
+.loop:
+    mov al, [rsi + rbx]
+    mov cl, [rdi + rbx]
+    cmp al, cl
+    jne .not_equal
+    test al, al
+    jz .equal
+    inc rbx
+    jmp .loop
+.equal:
+    xor rax, rax
+    pop rbx
+    ret
+.not_equal:
+    mov rax, 1
+    pop rbx
+    ret
+
+; Parse command line into argv array
+parse_args:
     lea rdi, [argv]
     lea rsi, [buf]
-    mov [rdi], rsi
+    mov [rdi], rsi      ; argv[0] = start of buffer
     xor rcx, rcx
-    mov rbx, 1
+    mov rbx, 1          ; argv index
 
-.build:
+.scan:
     cmp byte [buf + rcx], 0
-    je .finish
+    je .done
     cmp byte [buf + rcx], ' '
-    jne .next
+    jne .next_char
+    
+    ; Found space - null-terminate current arg
     mov byte [buf + rcx], 0
     inc rcx
+    
+    ; Skip multiple spaces
 .skip_spaces:
     cmp byte [buf + rcx], ' '
-    jne .set_arg
+    jne .new_arg
     inc rcx
     jmp .skip_spaces
-.set_arg:
+    
+.new_arg:
     cmp byte [buf + rcx], 0
-    je .finish
+    je .done
     lea rsi, [buf + rcx]
     mov [argv + rbx*8], rsi
     inc rbx
-    jmp .build
-.next:
-    inc rcx
-    jmp .build
-.finish:
-    mov qword [argv + rbx*8], 0
+    jmp .scan
 
-    mov rax, 57
+.next_char:
+    inc rcx
+    jmp .scan
+
+.done:
+    mov qword [argv + rbx*8], 0  ; NULL-terminate argv
+    ret
+
+; Execute external command using fork/exec
+execute_command:
+    mov rax, 57         ; sys_fork
     syscall
     test rax, rax
     jz .child
-    jl main_loop
-
+    jl .error
+    
+    ; Parent process - wait for child
     mov rdi, rax
-    mov rax, 61
+    mov rax, 61         ; sys_wait4
     xor rsi, rsi
     xor rdx, rdx
     xor r10, r10
     syscall
-    jmp main_loop
+    ret
 
 .child:
+    ; Child process - execute command
     mov rdi, [argv]
     lea rsi, [argv]
     xor rdx, rdx
-    mov rax, 59
+    mov rax, 59         ; sys_execve
     syscall
-
+    
+    ; If execve returns, command failed
     mov rax, 60
     mov rdi, 1
     syscall
 
-.do_clear:
-    mov rsi, clr
-    mov rdx, cll
-    call print
-    jmp main_loop
-
-.do_info:
-    mov rsi, kms
-    mov rdx, mkl
-    call print
-    jmp main_loop
-
-_exit:
-    mov rax, 60
-    xor rdi, rdi
-    syscall
+.error:
+    ret
